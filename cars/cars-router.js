b802f6011fcf24dbe400955b8501590e8efcdb2e
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-06-22 21:22:01 -0400

    only have to do post request

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -4,5 +4,24 @@ const db = require('../data/config')
 
 const router = express.Router()
 
+router.get("/", async (req,res,next)=>{
+    try{
+        const cars= await db("cars")
+        res.json(cars)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.get("/:id", async(req,res,next)=>{
+    try{
+        const {id}= req.params;
+        const car=await db("cars").where({id}).first()
+        res.json(car)
+    }catch(err){
+        next(err)
+    }
+
+})
 
 module.exports = router
