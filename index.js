f7099d4f244fd37e3bb9baeabd478ef2d882fa39
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-06-22 21:10:33 -0400

    working on seeds file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 //const helmet = require("helmet")
 
 const carsRouter = require("./cars/cars-router")
-
+const welcomeRouter = require("./welcome/welcome-router")
 const server = express()
 const port = process.env.PORT || 5000
 
@@ -11,6 +11,7 @@ server.use(express.json())
 
 
 server.use("/cars", carsRouter)
+server.use("/", welcomeRouter)
 
 server.use((err, req, res, next) => {
 	console.log(err)
